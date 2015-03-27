bad4187963eb8325a31d41f9424b4684552d8e3e
revouyt <tho.revouy@iut-valence.fr>
2015-03-27 17:05:25 +0100

    Compléter la classe principale,génération de la javadoc

--- src/PartieDeMasterMind.java
+++ src/PartieDeMasterMind.java
@@ -1,4 +1,28 @@
 
+/**
+ * classe qui représente le déroulement d'une partie de MasterMind
+ * @author revouyt
+ * 
+ */
 public class PartieDeMasterMind {
-
+	/**
+	 *  représente ce qu'on fait pendant la partie de MasterMind
+	 */
+	public void Jouer ()
+	{
+		while (joueur1.combinaison != joueur2.combinaison)
+		{
+		
+		}
+		
+		FinPartie();
+			
+	}
+	/**
+	 *  représente ce qui ce passe lors de la fin de la partie
+	 */
+	public void FinPartie()
+	{
+		
+	}
 }
